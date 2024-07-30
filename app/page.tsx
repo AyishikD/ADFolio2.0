8c168247a94a3a2c2dacd19b54929750cfe61b32
AyishikD <99983449+AyishikD@users.noreply.github.com>
2024-07-30 21:03:27 +0530

    Added Loader

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ const Home = () => {
 
   return (
     <>
-      <Loader loadingStates={loadingStates} loading={loading} duration={2000} />
+      <Loader loadingStates={loadingStates} loading={loading} duration={1000} />
       {!loading && (
         <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
           <div className="max-w-7xl w-full">
